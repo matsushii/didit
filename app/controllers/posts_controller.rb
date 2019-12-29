6d5a0a78de4abe29a126310a87c00b834006e7f1
matsushii <mtsmath24@yahoo.co.jp>
2019-12-30 00:28:34 +0900

    post.controllerに投稿に関するactionの設定
    post.controllerに投稿に関するaction(createとそのストロングパラメーター)を設定しました。

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,18 @@
 class PostsController < ApplicationController
   def index
   end
+
+  def create
+    @post = Post.new(post_params)
+    if @post.save
+      redirect_to root_path
+    else
+      redirect_to root_path
+    end
+  end
+
+  private
+  def post_params
+    params.require(:post).permit(:content)
+  end 
 end
