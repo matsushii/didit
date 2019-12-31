6208a53c78ebc79b5dbac1c29134ba9d3130705f
matsushii <mtsmath24@yahoo.co.jp>
2019-12-31 21:23:50 +0900

    paramsにcurrent_user.idを受け渡すよう記述
    paramsにcurrent_user.idの受け渡しができるようにストロングパラメーターにmerge(user_id: current_user.id）を追記しました

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -18,6 +18,6 @@ class PostsController < ApplicationController
 
   private
   def post_params
-    params.require(:post).permit(:content)
+    params.require(:post).permit(:content).merge(user_id: current_user.id)
   end 
 end
