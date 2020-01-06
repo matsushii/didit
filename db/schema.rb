57585faa7a5960991ac6e9607ff440a0be7faa4e
matsushii <mtsmath24@yahoo.co.jp>
2020-01-06 17:25:42 +0900

    commentsテーブルとmodel等の作成
    rails g model commentを用いてmodelおよびcommentsテーブルの作成を行いました。

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_30_170128) do
+ActiveRecord::Schema.define(version: 2020_01_06_081841) do
+
+  create_table "comments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
+    t.text "text", null: false
+    t.bigint "user_id"
+    t.bigint "post_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["post_id"], name: "index_comments_on_post_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
+  end
 
   create_table "posts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
     t.text "content", null: false
@@ -34,4 +44,6 @@ ActiveRecord::Schema.define(version: 2019_12_30_170128) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "comments", "posts"
+  add_foreign_key "comments", "users"
 end
