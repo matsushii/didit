000b6cca8fb56e471583a791ed0cb8225ab40a95
matsushii <mtsmath24@yahoo.co.jp>
2020-01-06 18:14:45 +0900

    routes.rbにて、commentをpostにネスト
    commentをpostにネストしました。

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "posts#index"
-  resources :posts, only: [:index, :create]
+  resources :posts do
+    resources :comments, only: [:create]
+  end
 end
