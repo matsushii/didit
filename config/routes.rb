1103c9d8ab44d6260d35a963b945210570b2e213
matsushii <mtsmath24@yahoo.co.jp>
2020-01-12 00:01:07 +0900

    likesのrouting（create, destroy）の設定
    likesのroutingの設定を行いました。

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   root to: "posts#index"
   resources :posts, only: [:index, :show, :create, :destroy] do 
     resources :comments, only: [:create]
+    resources :likes, only: [:create, :destroy]
   end
 end
