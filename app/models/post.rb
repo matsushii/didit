ca81f06db42391ed0710b0bdcdf1076eafbfdf46
matsushii <mtsmath24@yahoo.co.jp>
2020-01-15 20:53:24 +0900

    post.rbにcontentが1から100字以内になるようvalidationを記述
    post.rbにcontentが1から100字以内になるようvalidationを記述しました。

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,6 @@ class Post < ApplicationRecord
   belongs_to :user
   has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
+
+  validates :content, length: {in: 1..100}
 end
