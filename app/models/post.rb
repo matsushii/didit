1d7719729907ae4f03d773caed59211336afa688
matsushii <mtsmath24@yahoo.co.jp>
2020-01-16 19:34:22 +0900

    current_user.idをexists?で問うliked_byメソッドをpost.rbに記述
    同一userによる複数回のいいねを防ぐために、current_user.idをexists?で問うliked_byメソッドをpost.rbに記述

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,4 +4,8 @@ class Post < ApplicationRecord
   has_many :likes, dependent: :destroy
 
   validates :content, length: {in: 1..100}
+  
+  def liked_by(current_user)
+    likes.where(user_id: current_user.id).exists?
+  end
 end
