7ea172829de06613eafcc499a36fb1c84dfac850
matsushii <mtsmath24@yahoo.co.jp>
2020-01-16 23:38:45 +0900

    likes_controllerにaction:destroy &createにnoticeを追記
    いいねの削除のために、likes_controllerにaction:destroyを追記しました。また、createにnoticeを追記しました。

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -2,9 +2,19 @@ class LikesController < ApplicationController
   def create
     @like = current_user.likes.new(post_id: params[:post_id])
     if @like.save
-      redirect_to root_path
+      redirect_to root_path, notice: "投稿にいいねしました"
     else
       redirect_to root_path
     end
   end
+
+  def destroy
+    @like = current_user.likes.find_by(post_id: params[:post_id])
+    if  @like.destroy
+      redirect_to root_path, notice: "いいねを取り消しました"
+    else
+      redirect_to root_path
+    end
+  end
+
 end
