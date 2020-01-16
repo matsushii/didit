4b9b4c19dda74fef43e799519e18a760a946e64f
matsushii <mtsmath24@yahoo.co.jp>
2020-01-16 23:44:40 +0900

    post_controllerへaction:indexでN+1問題の回避をするために、includes(:user)を追記
    post_controllerへaction:indexでN+1問題の回避をするために、includes(:user)を追記した。

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,7 +2,7 @@ class PostsController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @posts = Post.all.order(created_at: "DESC")
+    @posts = Post.all.order(created_at: "DESC").includes(:user).limit(20)
   end
 
   def create
