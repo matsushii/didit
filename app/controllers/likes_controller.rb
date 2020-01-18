29219a5449d00e66d5df1d353ccaf3fc1499951a
matsushii <mtsmath24@yahoo.co.jp>
2020-01-18 22:12:56 +0900

    いいね時のfrashをnoticeからalertに変更
    いいね時のfrashをnoticeからalertに変更しました。

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -11,7 +11,7 @@ class LikesController < ApplicationController
   def destroy
     @like = current_user.likes.find_by(post_id: params[:post_id])
     if  @like.destroy
-      redirect_to root_path, notice: "いいねを取り消しました"
+      redirect_to root_path, alert: "いいねを取り消しました"
     else
       redirect_to root_path
     end
