530a0ee657e6263f91fd9d83d78879c890575308
matsushii <mtsmath24@yahoo.co.jp>
2020-01-19 07:36:29 +0900

    create,destroy.jsの削除
    create, destroy.jsを削除しました。

--- app/javascript/likes/create.js
+++
@@ -1,3 +0,0 @@
-$("#post__bottom__reaction__like--blue").html("<%= j(render partial: 'like') %>")
-
-// jはescape_javascriptのエイリアス
--- app/javascript/likes/destroy.js
+++
@@ -1,3 +0,0 @@
-$("#post__bottom__reaction__like--white").html("<%= j(render partial: 'like') %>")
-
-// jはescape_javascriptのエイリアス
