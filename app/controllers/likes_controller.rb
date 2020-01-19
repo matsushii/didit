4ba079e4e58079ba2918afbccd7efb5080966336
matsushii <mtsmath24@yahoo.co.jp>
2020-01-19 13:32:50 +0900

    like_controllerにset_postおよびstrong_parameterを追記
    like_controllerにset_postおよびstrong_parameterのlike_paramsを追記しました。

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,9 +1,10 @@
 class LikesController < ApplicationController
+  before_action :set_post
+
   def create
-    @like = current_user.likes.new(post_id: params[:post_id])
+    @like = current_user.likes.new(like_params)
     if @like.save
       respond_to do |format|
-        format.json
         format.js
       end
     else
@@ -14,10 +15,20 @@ class LikesController < ApplicationController
   def destroy
     @like = current_user.likes.find_by(post_id: params[:post_id])
     if  @like.destroy
-      redirect_to root_path, alert: "いいねを取り消しました"
+      respond_to do |format|
+        format.js
+      end
     else
-      redirect_to root_path
+      redirect_to root_path, alert: "いいねを取り消せません"
     end
   end
 
+  private
+  def set_post
+    @post = Post.find(params[:post_id])    
+  end
+  
+  def like_params
+    params.permit(:post_id)
+  end
 end
