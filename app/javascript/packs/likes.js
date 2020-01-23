fb195672f2ee6fb847e428d29b7c7c33b5040d58
matsushii <mtsmath24@yahoo.co.jp>
2020-01-23 20:35:56 +0900

    likes.jsのコメントアウト
    likes.jsをコメントアウトしました。理由は、js.erbでの実装に切り替えるからである。jsでの実装が可能になり次第、jsへ切り替える予定です。

--- app/javascript/packs/likes.js
+++ app/javascript/packs/likes.js
@@ -1,43 +1,40 @@
-$(document).on('turbolinks:load', function(){
+// $(document).on('click', "#like_botton", function (){
+//   console.log(this);
+//   var post_id = $(this).data('post-id');
+//   console.log( post_id );  
+//   var like_id = $(this).data('like-id');
+//   console.log( like_id );  
+//   var result = $('#like_botton').find('.fas.fa-thumbs-up');
 
-  $('#like_botton').on('click', function (){
-    console.log(this);
-    var post_id = $(this).data('post-id');
-    console.log( post_id );  
-    var like_id = $(this).data('like-id');
-    console.log( like_id );  
-    var result = $('#like_botton').find('.fas.fa-thumbs-up');
+//   if (result.length) {
+//     $.ajax ({
+//       url:  '/posts/'+ post_id +'/likes/' + like_id,
+//       type: 'DELETE',
+//       data: {post_id: post_id, id: like_id},
+//       dataType: 'JSON',
+//       processData: false,
+//       contentType: false
+//     })
 
-    if (result.length) {
-      $.ajax ({
-        url:  '/posts/'+ post_id +'/likes/' + like_id,
-        type: 'DELETE',
-        data: {post_id: post_id, id: like_id},
-        dataType: 'JSON',
-        processData: false,
-        contentType: false
-      })
-  
-      .done(function(data){
-        console.log("delete");
-        $('#like_botton').empty();
-        $('#like_botton').append('<i class="far fa-thumbs-up"></i>');
-      });
-    } else {
-      $.ajax ({
-        url:  '/posts/'+ post_id +'/likes',
-        type: 'POST',
-        data: {post_id: post_id},
-        dataType: 'JSON',
-        processData: false,
-        contentType: false
-      })
-  
-      .done(function(post){
-        console.log("create");
-        $('#like_botton').empty();
-        $('#like_botton').append('<i class="fas fa-thumbs-up"></i>');
-      });
-    }
-  });
-})
+//     .done(function(data){
+//       console.log("delete");
+//       $('#like-' + post_id).empty();
+//       $('#like-' + post_id).append('#like-' + post_id );
+//     });
+//   } else {
+//     $.ajax ({
+//       url:  '/posts/'+ post_id +'/likes',
+//       type: 'POST',
+//       data: {post_id: post_id},
+//       dataType: 'JSON',
+//       processData: false,
+//       contentType: false
+//     })
+
+//     .done(function(post){
+//       console.log("create");
+//       $('#like-' + post_id).empty();
+//       $('#like-' + post_id).append('j(render )');    
+//     });
+//   }
+// });
