c7ad3ee269d2ad52372cbee02405987f1cee9e8e
matsushii <mtsmath24@yahoo.co.jp>
2020-01-24 19:16:25 +0900

    post_formのfadeIn, fadeOutを行うmodal.jsの作成
    post_formのfadeIn, fadeOutを行うmodal.jsの作成を行いました。

---
+++ app/javascript/packs/modal.js
@@ -0,0 +1,8 @@
+$(document).on('turbolinks:load', function(){
+  $('#bottom__form_btn').on('click', function() {
+    $('#overlay, #modal_window').fadeIn(800);
+  });
+  $('#window_close').on('click', function() {
+    $('#overlay, #modal_window').fadeOut(800);
+  });
+}); 
