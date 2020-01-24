4da6a54b986729659414a38fb9d8a239f1f10f65
matsushii <mtsmath24@yahoo.co.jp>
2020-01-24 19:17:07 +0900

    application.scssにimport modal.jsの追記
    application.scssにimport modal.jsの追記しました.

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -11,6 +11,8 @@ require('jquery')
 
 import "./notice";
 import "./likes";
+import "./modal";
+
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
