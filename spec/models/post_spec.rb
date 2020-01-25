34618d8289898ed8752a50e41d29db0f48c853ec
matsushii <mtsmath24@yahoo.co.jp>
2020-01-25 22:45:27 +0900

    投稿内容が101字以上だと投稿できないテストコードを記述
    投稿内容が101字以上だと投稿できないテストコードを記述しました。

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -21,5 +21,12 @@ describe Post do
       expect(post).to be_valid
     end
 
+    it "投稿内容が101字以上であれば投稿できないこと" do
+      user = create(:user)
+      post = build(:post, content: "あるニワトリ小屋で、飼育員が毎日、エサを決まった時間に同じ量だけを与えていた。飼育員は、非常に几帳面な性格だったらしく、何年間も正確に同じことをしていた。さて、小屋の中のニワトリたちは、なぜ、毎日同じ", user_id: user.id)
+      post.valid?
+      expect(post.errors[:content])
+    end
+
   end
 end
