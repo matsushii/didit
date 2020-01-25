8a0df6c097afbfea33642d74e6be03cdbee325b8
matsushii <mtsmath24@yahoo.co.jp>
2020-01-25 22:43:05 +0900

    100字以内であれば投稿ができるテストコードを記述
    100字以内であれば投稿ができるテストコードを記述しました。

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -14,5 +14,12 @@ describe Post do
       post.valid?
       expect(post.errors[:content])
     end
+
+    it "投稿内容が100字以内であれば投稿できる" do
+      user = create(:user)
+      post = build(:post, content: "あるニワトリ小屋で、飼育員が毎日、エサを決まった時間に同じ量だけを与えていた。飼育員は、非常に几帳面な性格だったらしく、何年間も正確に同じことをしていた。さて、小屋の中のニワトリたちは、なぜ、毎日同じ", user_id: user.id)
+      expect(post).to be_valid
+    end
+
   end
 end
