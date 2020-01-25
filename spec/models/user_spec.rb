7c2f3caa92a3538722fe46d3bdb0dc2ee3003138
matsushii <mtsmath24@yahoo.co.jp>
2020-01-25 19:40:36 +0900

    ニックネームが空の場合に登録できないテストコードを記述
    ニックネームが空の場合に登録できないテストコードを記述しました。

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,8 +3,14 @@ describe User do
   describe '#create' do
     it "ニックネーム、パスワード、パスワード確認用が全て正常に入力されている時" do
       user = build(:user)
-      user.valid?
       expect(user).to be_valid
     end
+    
+    it "ニックネームが空では登録ができない" do
+      user = build(:user, nickname: nil)
+      user.valid?
+      expect(user.errors[:nickname]).to include("は1文字以上で入力してください")
+    end
+
   end
 end
