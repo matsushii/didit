ba916adb34a5604d498ddaf4a05386d9eda8ef8c
matsushii <mtsmath24@yahoo.co.jp>
2020-01-25 23:27:07 +0900

    複数回のいいねはできないテストコードを記述
    複数回のいいねはできないことを確認するためのテストコードを記述しました。

--- spec/models/like_spec.rb
+++ spec/models/like_spec.rb
@@ -7,5 +7,13 @@ describe Like do
       like = build(:like, post_id: post.id, user_id: user.id)
       expect(like).to be_valid
     end
+      it "複数回のいいねはできない" do
+        user = create(:user)
+        post = create(:post, user_id: user.id)
+        like = create(:like, post_id: post.id, user_id: user.id)
+        like = build(:like, post_id: post.id, user_id: user.id)
+        like.valid?
+        expect(like.errors)
+      end
   end
 end
