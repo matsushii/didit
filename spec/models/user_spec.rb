d4d2ed17e65780d9a208d358620e8e34188e6b6b
matsushii <mtsmath24@yahoo.co.jp>
2020-01-25 19:42:06 +0900

    パスワードの入力欄がからの場合に登録できないテストコードを記述
    パスワードの入力欄がからの場合に登録できないテストコードを記述しました。

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,12 +5,17 @@ describe User do
       user = build(:user)
       expect(user).to be_valid
     end
-    
+
     it "ニックネームが空では登録ができない" do
       user = build(:user, nickname: nil)
       user.valid?
       expect(user.errors[:nickname]).to include("は1文字以上で入力してください")
     end
 
+    it "パスワードが空では登録ができない" do
+      user = build(:user, password: nil)
+      user.valid?
+      expect(user.errors[:password]).to include("を入力してください")
+    end
   end
 end
