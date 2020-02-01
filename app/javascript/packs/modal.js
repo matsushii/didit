18ebe1c1ae25ae22a0859ce456f3d9e5632c6efe
matsushii <mtsmath24@yahoo.co.jp>
2020-02-02 00:22:38 +0900

    modal_windowのclose_action指定にoverlayを追加
    modal_windowのclose_action指定にoverlayを追加し、closeしやすくしました。

--- app/javascript/packs/modal.js
+++ app/javascript/packs/modal.js
@@ -2,7 +2,7 @@ $(document).on('turbolinks:load', function(){
   $('#bottom__form_btn').on('click', function() {
     $('#overlay, #modal_window').fadeIn(800);
   });
-  $('#window_close').on('click', function() {
+  $('#overlay, #window_close').on('click', function() {
     $('#overlay, #modal_window').fadeOut(800);
   });
 }); 
