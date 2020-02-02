b175c2b81a592326d0f51831d51a17c66f1b0c7c
matsushii <mtsmath24@yahoo.co.jp>
2020-02-02 23:01:05 +0900

    formをclickするとmodal.jsのcloseが作動するのを回避
    formをclickするとmodal.jsのcloseが作動するのを回避するために、#window_closeのみに戻しました。

--- app/javascript/packs/modal.js
+++ app/javascript/packs/modal.js
@@ -2,7 +2,7 @@ $(document).on('turbolinks:load', function(){
   $('#bottom__form_btn').on('click', function() {
     $('#overlay, #modal_window').fadeIn(800);
   });
-  $('#overlay, #window_close').on('click', function() {
+  $('#window_close').on('click', function() {
     $('#overlay, #modal_window').fadeOut(800);
   });
 }); 
